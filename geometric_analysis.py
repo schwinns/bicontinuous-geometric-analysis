9e312e85544f2839176128956560a56749628e10
Nate Schwindt <nathanael.schwindt@colorado.edu>
2022-01-21 10:04:13 -0700

    added brentq bracketed solver

--- geometric_analysis.py
+++ geometric_analysis.py
@@ -122,14 +122,14 @@ def surface2surface(structure,struct='gyroid',guess=4.5,box=9.4,period=9.4,sampl
     
     # some hard coded constants
     short_dist_tol = 0.01
-    small_tol = 1
+    small_tol = 0.01
 
     # intialize all variables
     distribution = []
-    neg = 0
-    small = 0
-    big = 0
-    good = 0
+    neg = []
+    small = []
+    big = []
+    good = []
 
     # define necessary functions for the chosen structure
     if struct == 'gyroid':
@@ -156,6 +156,67 @@ def surface2surface(structure,struct='gyroid',guess=4.5,box=9.4,period=9.4,sampl
         sol = fsolve(solve_eq(new_point,period), guess)
         
         if restrictions:
+            if abs(sol) < box and abs(sol) > small_tol: # only save reasonable values
+                distribution.append(abs(sol))
+        else:
+            distribution.append(sol) # save every value
+
+        # keep track of the abnormalities
+        if abs(sol) < box and abs(sol) > small_tol:
+            good.append(new_point)
+        if sol < 0 and abs(sol) > small_tol:
+            neg.append(new_point)
+            point2 = P(0.001,new_point,period=period)
+            sol2 = fsolve(solve_eq(point2,period), guess)
+            print('Point %s' %(new_point))
+            print('\tOriginal: %.4f' %(sol) )
+            print('\tResolved: %.4f' %(sol2) )
+        if abs(sol) < small_tol:
+            small.append(new_point)
+        if abs(sol) > box:
+            big.append(new_point)
+                
+    print('\nReasonable solutions for %s: %d' %(struct, len(good)))
+    print('Negative solutions for %s: %d' %(struct, len(neg)))
+    print('Solutions < %s nm for %s: %d' %(small_tol, struct, len(small)))
+    print('Solutions outside the box for %s: %d' %(struct, len(big)))
+    
+    return distribution
+
+def old_algorithm(structure,struct='gyroid',guess=4.5,box=9.4,period=9.4,sample=10,restrictions=True):
+    
+    # some hard coded constants
+    short_dist_tol = 0.01
+    small_tol = 1
+
+    # intialize all variables
+    distribution = []
+    neg = 0
+    small = 0
+    big = 0
+    good = 0
+
+    # define necessary functions for the chosen structure
+    if struct == 'gyroid':
+        P = P_gyroid
+        solve_eq = Gyroid_eq
+    elif struct == 'schwarzD':
+        P = P_schwarz
+        solve_eq = SchwarzD_eq
+    elif struct == 'primitive':
+        P = P_primitive
+        solve_eq = Primitive_eq
+
+    # generate distribution of surface to surface distances
+    while len(distribution) < sample:
+
+        p = random.randint(0, structure.shape[0] - 1) # choose a random point on the discretized surface
+        point = structure[p,:]
+
+        # solve for the distance from the new point to another point on the minimal surface
+        sol = fsolve(solve_eq(point,period), guess)
+        
+        if restrictions:
             if abs(sol) < box and abs(sol) > 1: # only save reasonable values
                 distribution.append(abs(sol))
         else:
@@ -177,6 +238,102 @@ def surface2surface(structure,struct='gyroid',guess=4.5,box=9.4,period=9.4,sampl
     print('Solutions outside the box for %s: %d' %(struct, big))
     
     return distribution
+
+def brentq_solver(structure,struct='gyroid',box=9.4,period=9.4,sample=10,restrictions=True):
+    
+    # some hard coded constants
+    short_dist_tol = 0.01
+    small_tol = 0.01
+    upper = box
+    lower = small_tol
+    checked = False
+
+    # intialize all variables
+    distribution = []
+    neg = []
+    small = []
+    big = []
+    good = []
+
+    # define necessary functions for the chosen structure
+    if struct == 'gyroid':
+        P = P_gyroid
+        solve_eq = Gyroid_eq
+    elif struct == 'schwarzD':
+        P = P_schwarz
+        solve_eq = SchwarzD_eq
+    elif struct == 'primitive':
+        P = P_primitive
+        solve_eq = Primitive_eq
+
+    # generate distribution of surface to surface distances
+    while len(distribution) < sample:
+
+        if not checked:
+            p = random.randint(0, structure.shape[0] - 1) # choose a random point on the discretized surface
+            point = structure[p,:]
+            
+            # find the point actually on the surface
+            short_dist = fsolve(solve_eq(point,period), short_dist_tol)
+            new_point = P(short_dist, point, period=period)
+
+        # solve for the distance from the new point to another point on the minimal surface
+        dt = 0.01
+        a = None; b = None
+        n_sol = False; p_sol = False
+        for t in np.arange(lower,upper,dt):
+            if t < -2*small_tol or t > 2*small_tol:
+                f = solve_eq(new_point, period=period)(t)
+                if f < 0:
+                    a = t
+                    n_sol = True
+                if f > 0:
+                    b = t
+                    p_sol = True
+                if n_sol and p_sol:
+                   break
+
+        if not n_sol or not p_sol:
+            if not checked:
+                # print('Could not find a bracket for the solution... Changing the search range.')
+                lower = -box
+                upper = -small_tol
+                checked = True
+            else:
+                print('Could not find a bracket for the solution... Exiting.')
+                exit()
+        elif checked:
+            lower = small_tol
+            upper = box
+            checked = False
+            
+        if not checked:
+            sol = brentq(solve_eq(new_point,period), a=a, b=b)
+            # print(sol)
+
+            if restrictions:
+                if abs(sol) < box and abs(sol) > small_tol: # only save reasonable values
+                    distribution.append(abs(sol))
+            else:
+                distribution.append(sol) # save every value
+
+            # keep track of the abnormalities
+            if abs(sol) < box and abs(sol) > small_tol:
+                good.append(new_point)
+            if sol < 0 and abs(sol) > small_tol:
+                neg.append(new_point)
+            if abs(sol) < small_tol:
+                small.append(new_point)
+            if abs(sol) > box:
+                big.append(new_point)
+                
+    print('\nReasonable solutions for %s: %d' %(struct, len(good)))
+    print('Negative solutions for %s: %d' %(struct, len(neg)))
+    print('Solutions < %s nm for %s: %d' %(small_tol, struct, len(small)))
+    print('Solutions outside the box for %s: %d' %(struct, len(big)))
+    
+    return distribution
+
 
 '''
 # Solve using different solvers instead of fsolve (Newton)
@@ -245,7 +402,7 @@ parser.add_argument('-s','--struct',nargs='+',
 parser.add_argument('-n','--sample',type=int,
                     help='number of samples in each distribution')
 parser.add_argument('-r','--restrict',action='store_true',
-                    help='only save reasonable values')
+                    help='only save reasonable values, default = False')
 parser.add_argument('-o','--output',default='output.png',
                     help='name of the output figure file')
 args = parser.parse_args()
@@ -267,6 +424,11 @@ if 'primitive' in args.struct:
     primitive = True
 else:
     primitive = False
+# sample = 100
+# restrictions = False
+# gyroid = False
+# schwarz = True
+# primitive = False
 
 # Some hard coded parameters to use in all distributions
 n = 100                 # grid size for discretized structure
@@ -274,7 +436,7 @@ box = 9.4               # box size in nm
 period = box            # period of the minimal surface in nm
 struct_tol = 0.01       # tolerance for locating discretized points on the surface
 guess = 4.6             # initial guess for the numerical solvers --> corresponds to the bilayer distance + expected pore size from MD and experiment
-# random.seed(123)      # uncomment if you want to set a random seed for reproducibility
+random.seed(123)      # uncomment if you want to set a random seed for reproducibility
 
 
 ##################################################################################################################
@@ -303,7 +465,8 @@ if gyroid:
     structure = grid[:count, :]
 
     # Generate the distribution
-    dist_gyroid = surface2surface(structure,struct='gyroid',guess=guess,box=box,period=period,sample=sample,restrictions=restrictions)
+    # dist_gyroid = surface2surface(structure,struct='gyroid',guess=guess,box=box,period=period,sample=sample,restrictions=restrictions)
+    dist_gyroid = brentq_solver(structure,struct='gyroid',box=box,period=period,sample=sample,restrictions=restrictions)
     hist_gyroid = pd.DataFrame(dist_gyroid,columns=['Gyroid'])
 
 if schwarz:
@@ -328,7 +491,8 @@ if schwarz:
     structure = grid[:count, :]
 
     # Generate the distribution
-    dist_schwarzD = surface2surface(structure,struct='schwarzD',guess=guess,box=box,period=period,sample=sample,restrictions=restrictions)
+    #dist_schwarzD = surface2surface(structure,struct='schwarzD',guess=guess,box=box,period=period,sample=sample,restrictions=restrictions)
+    dist_schwarzD = brentq_solver(structure,struct='schwarzD',box=box,period=period,sample=sample,restrictions=restrictions)
     hist_schwarzD = pd.DataFrame(dist_schwarzD,columns=['SchwarzD'])
 
 
@@ -354,28 +518,29 @@ if primitive:
     structure = grid[:count, :]
 
     # Generate the distribution
-    dist_primitive = surface2surface(structure,struct='primitive',guess=guess,box=box,period=period,sample=sample,restrictions=restrictions)
+    # dist_primitive = surface2surface(structure,struct='primitive',guess=guess,box=box,period=period,sample=sample,restrictions=restrictions)
+    dist_primitive = brentq_solver(structure,struct='primitive',box=box,period=period,sample=sample,restrictions=restrictions)
     hist_primitive = pd.DataFrame(dist_primitive,columns=['Primitive'])
 
 # Plot histograms with matplotlib
 if restrictions:
     bins = np.linspace(0,10,50)
 else: # get minimums and maximums for histogram bins
-    min_dist = 100
-    max_dist = 0
-    if gyroid and min_dist > min(dist_gyroid):
-        min_dist = min(dist_gyroid)
-    if schwarz and min_dist > min(dist_schwarzD):
-        min_dist = min(dist_schwarzD)
-    if primitive and min_dist > min(dist_primitive):
-        min_dist = min(dist_primitive)
+    min_dist = [100]
+    max_dist = [0]
+    if gyroid and min_dist[0] > min(dist_gyroid):
+        min_dist[0] = min(dist_gyroid)
+    if schwarz and min_dist[0] > min(dist_schwarzD):
+        min_dist[0] = min(dist_schwarzD)
+    if primitive and min_dist[0] > min(dist_primitive):
+        min_dist[0] = min(dist_primitive)
 
-    if gyroid and max_dist < max(dist_gyroid):
-        max_dist = max(dist_gyroid)
-    if schwarz and max_dist < max(dist_schwarzD):
-        max_dist = max(dist_schwarzD)
-    if primitive and max_dist > max(dist_primitive):
-        max_dist = max(dist_primitive)
+    if gyroid and max_dist[0] < max(dist_gyroid):
+        max_dist[0] = max(dist_gyroid)
+    if schwarz and max_dist[0] < max(dist_schwarzD):
+        max_dist[0] = max(dist_schwarzD)
+    if primitive and max_dist[0] > max(dist_primitive):
+        max_dist[0] = max(dist_primitive)
 
     min_dist = min_dist[0]
     max_dist = max_dist[0]
@@ -408,7 +573,7 @@ ax.set_xticks(np.arange(0,11,1))
 ax.set_ylim(0,args.sample/3)
 ax.set_xlabel('distance (nm)',fontsize='large')
 ax.set_ylabel('counts',fontsize='large')
-ax.legend(fontsize='x-large')
+ax.legend(fontsize='x-large',loc=1)
 title = 'Theoretical pore center to pore center distances for BCC structures, guess = %s' %(guess)
 ax.set_title(title)
 fig.savefig(args.output)
@@ -428,15 +593,16 @@ if not restrictions:
 
 
     # Add the bilayer thickness + pore size line
-    x = np.ones(10)*4.59
-    y = np.linspace(0,args.sample,10)
-    ax.plot(x,y, color='black', linestyle='dashed',label='Expected pore-to-pore distance')
-    ax.axvspan(4.59 - 0.17, 4.59 + 0.17, color='gray', alpha=0.5)
+    # x = np.ones(10)*4.59
+    # y = np.linspace(0,args.sample,10)
+    # ax.plot(x,y, color='black', linestyle='dashed',label='Expected pore-to-pore distance')
+    # ax.axvspan(4.59 - 0.17, 4.59 + 0.17, color='gray', alpha=0.5)
 
-    ax.set_ylim(0,args.sample/10)
+    ax.set_xlim(-box,2*box)
+    # ax.set_ylim(0,args.sample/10)
     ax.set_xlabel('distance (nm)',fontsize='large')
     ax.set_ylabel('counts',fontsize='large')
-    ax.legend(fontsize='x-large')
+    ax.legend(fontsize='x-large',loc=1)
     title = 'Theoretical pore center to pore center distances for BCC structures, guess = %s' %(guess)
     ax.set_title(title)
 
